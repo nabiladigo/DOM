b60c73bfb418aa0392e9998bc9bbc4d94b3abb09
Eric Fithian <ef2413@columbia.edu>
2021-12-22 11:03:44 -0500

    Updated with the DOM manipulation

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,11 @@
-let myVariable = document.querySelector('ul');
+const btn = document.querySelector('button');
 
+function handleClick (event) {
+    const myLi = document.createElement('li');
+    const input = document.querySelector('input');
+    myLi.innerHTML = input.value;
+    input.value = '';
+    document.querySelector('ul').appendChild(myLi);
+}
+
+btn.addEventListener('click', handleClick)
